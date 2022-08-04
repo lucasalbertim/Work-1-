210d6fb93e79ce7193f2fd7846dfd39e1a9777ec
Lucas <lucas.albertim7@gmail.com>
2022-08-04 12:29:37 -0300

    Adicionado Validações em campos essenciais.

--- Program.cs
+++ Program.cs
@@ -19,9 +19,23 @@ namespace MyWork
             Console.WriteLine("Por favor informe o nome da Conta");
             string nome = Console.ReadLine();
 
-            Console.WriteLine("Qual o CNPJ?");
-            string cnpj = Console.ReadLine();
+            string cnpjconta = null;
+            bool valida = false;
+            while (valida == false)
+            {
+                Console.WriteLine("Qual o CNPJ?");
+                cnpjconta = Console.ReadLine();
+                var v = ValidacoesLibrary.Validacoes.VerificaCnpj(cnpjconta);
 
+                if (v != false)
+                {
+                    valida = true;
+                }
+                else
+                {
+                    Console.WriteLine("CNPJ invalido, favor preencher novamente!");
+                }
+            }
             Console.WriteLine("Qual o telefone principal?");
             string telefone = Console.ReadLine();
 
@@ -34,113 +48,133 @@ namespace MyWork
             Console.WriteLine("Qual o porte da empresa? 1 - Pequeno 2 - Média 3 - Grande");
             int porte = Convert.ToInt32(Console.ReadLine());
 
-           /* Console.WriteLine("Qual o seu continente ?");
-            Console.WriteLine("1 - Americano?");
-            Console.WriteLine("2 - Asia?");
-            Console.WriteLine("3 - Áfricano?");
-            Console.WriteLine("4 - Europeu?");
-            Console.WriteLine("5 - Oceania?");
-            Console.WriteLine("6 -  Antártida?");
+            Console.WriteLine("Qual o seu continente ?");
+            Console.WriteLine("1 - Americano");
+            Console.WriteLine("2 - Asia");
+            Console.WriteLine("3 - Áfricano");
+            Console.WriteLine("4 - Europeu");
+            Console.WriteLine("5 - Oceania");
+            Console.WriteLine("6 - Antártida");
             int cont = Convert.ToInt32(Console.ReadLine());
+            Guid continente = new Guid();
 
 
-            string continente = null;
-            if (cont == 1)
+            switch (cont)
             {
-                continente = "Americano";
+                case 1:
+                    continente = Guid.Parse("dec81876-9c13-ed11-b83e-000d3ac12d88");
+                    break;
+                case 2:
+                    continente = Guid.Parse("320a012c-b113-ed11-b83e-000d3ac12d88");
+                    break;
+                case 3:
+                    continente = Guid.Parse("eb7a1e8e-9c13-ed11-b83e-000d3ac12d88");
+                    break;
+                case 4:
+                    continente = Guid.Parse("f2e73781-9c13-ed11-b83e-000d3ac12d88");
+
+                    break;
+                case 5:
+                    continente = Guid.Parse("4b8028a7-9c13-ed11-b83e-000d3ac12d88");
+                    break;
+                case 6:
+                    continente = Guid.Parse("ac050dae-9c13-ed11-b83e-000d3ac12d88");
+                    break;
             }
-            else if (cont == 2)
-            {
-                continente = "Ásia";
-            }
-            else if (cont == 3)
-            {
-                continente = "Africano";
-            }
-            else if (cont == 4)
-            {
-                continente = "Europeu";
-            }
-            else if (cont == 5)
-            {
-                continente = "Oceania";
-            }
-            else if (cont == 6)
-            {
-                continente = "Antártida";
-            }
-            else
-            {
-                Console.WriteLine("Valor invalido, tente novamente");
-            }*/
 
             Entity conta = new Entity("account");
 
             conta["name"] = nome.ToString();
-            conta["la_cnpj"] = cnpj.ToString();
+            conta["la_cnpj"] = cnpjconta.ToString();
             conta["telephone1"] = telefone.ToString();
             conta["emailaddress1"] = email.ToString();
             conta["la_faturamentoanual"] = new Money(faturamento);
             conta["la_portedaempresa"] = new OptionSetValue(porte);
-           // conta["la_continente"] = new EntityReference("la_name", continente);
+            conta["la_continente"] = new EntityReference("la_continentes", continente);
 
             Guid accountId = service.Create(conta);
 
-            Console.WriteLine("Você deseja criar um contato para essa conta? (S/N)");
-            string resposta = Console.ReadLine();
-            string upresposta = resposta.ToUpper();
-            if (upresposta == "S")
+            valida = false;
+            while (valida == false)
             {
+                Console.WriteLine("Você deseja criar um contato para essa conta? (S/N)");
+                string resposta = Console.ReadLine();
+                string upresposta = resposta.ToUpper();
 
-                Console.WriteLine("Por favor informe o primeiro nome do contato");
-                string primeironome = Console.ReadLine();
+                if (upresposta == "S" || upresposta == "N")
+                {
 
-                Console.WriteLine("Por favor informe o segundo nome do contato");
-                string segundonome = Console.ReadLine();
+                    switch (upresposta)
+                    {
+                        case "S":
+                            valida = true;
+                            Console.WriteLine("Por favor informe o primeiro nome do contato");
+                            string primeironome = Console.ReadLine();
 
-                Console.WriteLine("Qual o CPF?");
-                string CPF = Console.ReadLine();
+                            Console.WriteLine("Por favor informe o segundo nome do contato");
+                            string segundonome = Console.ReadLine();
 
-                Console.WriteLine("Qual o email do contato?");
-                string emailcontato = Console.ReadLine();
+                            string cpf = null;
+                            valida = false;
+                            while (valida == false)
+                            {
+                                Console.WriteLine("Qual o CPF?");
+                                cpf = Console.ReadLine();
+                                var v = ValidacoesLibrary.Validacoes.ValidaCPF(cpf);
 
-                Console.WriteLine("Qual o cargo?");
-                string cargo = Console.ReadLine();
+                                if (v != false)
+                                {
+                                    valida = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("CPF invalido, favor preencher novamente!");
+                                }
+                            }
+                            Console.WriteLine("Qual o email do contato?");
+                            string emailcontato = Console.ReadLine();
 
-                Console.WriteLine("Qual o sexo?");
-                Console.WriteLine("1 - Masculino?");
-                Console.WriteLine("2 - Feminino");
-                int sexo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual o cargo?");
+                            string cargo = Console.ReadLine();
 
-                Console.WriteLine("Qual o seu limite de credito ??");
-                decimal limitecredito = Convert.ToDecimal(Console.ReadLine());
+                            Console.WriteLine("Qual o sexo?");
+                            Console.WriteLine("1 - Masculino?");
+                            Console.WriteLine("2 - Feminino");
+                            int sexo = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-                Entity contato = new Entity("contact");
+                            Console.WriteLine("Qual o seu limite de credito ?");
+                            decimal limitecredito = Convert.ToDecimal(Console.ReadLine());
 
 
-                contato["firstname"] = primeironome.ToString();
-                contato["lastname"] = segundonome.ToString();
-                contato["jobtitle"] = cargo.ToString();
-                contato["gendercode"] = new OptionSetValue(sexo);
-                contato["emailaddress1"] = emailcontato.ToString();
-                contato["creditlimit"] = new Money(limitecredito);
-                contato["parentcustomerid"] = new EntityReference("account", accountId);
+                            Entity contato = new Entity("contact");
 
-                service.Create(contato);
+                            contato["firstname"] = primeironome.ToString();
+                            contato["lastname"] = segundonome.ToString();
+                            contato["jobtitle"] = cargo.ToString();
+                            contato["la_cpf"] = cpf.ToString();
+                            contato["emailaddress1"] = emailcontato.ToString();
+                            contato["gendercode"] = new OptionSetValue(sexo);
+                            contato["creditlimit"] = new Money(limitecredito);
+                            contato["parentcustomerid"] = new EntityReference("account", accountId);
 
+                            service.Create(contato);
+                            break;
+                        case "N":
+                            valida = true;
+                            Environment.Exit(0);
+                            break;
+
+
+
+
+
+                    }
+                }
+                else 
+                { 
+                    Console.WriteLine("Resposta Invalida, por favor tente novamente!");
+                }
             }
-            else if (upresposta == "N")
-            {
-                Environment.Exit(0);
-            }
-            else
-            {
-                return;
-            }
-
         }
     }
 }
